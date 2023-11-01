6a28819a051c78cf5f54cdda70f1681fa32bae63
Maiko Duarte <mkdu185@gmail.com>
2023-11-01 20:26:02 -0300

    Adicionando dictionary e streamWriter

--- Program.cs
+++ Program.cs
@@ -1,38 +1,65 @@
-﻿Console.WriteLine("Digite o seu nome:");
-string nome = Console.ReadLine();
-
-DateTime? dataNascimento = null; 
-
-while (dataNascimento == null)
+﻿try
 {
-  Console.WriteLine("Digite a data de nascimento no formato (dd/MM/yyyy, ddMMyy ou ddMMyyyy):");
-  string dataDeNascimento = Console.ReadLine();
+    Dictionary<string, DateTime> dadosPessoais = new Dictionary<string, DateTime>();
+    int contadorPessoas = 0;
 
-  if (string.IsNullOrWhiteSpace(dataDeNascimento))
-  {
-    Console.WriteLine("A data de nascimento não pode estar em branco. Tente novamente.");
-    continue; // Volta a pedir a data de nascimento
-  }
+  
+    while (contadorPessoas < 3)
+    {
+        Console.WriteLine("Digite o seu nome:");
+        string nome = Console.ReadLine()!;
+        
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            Console.WriteLine("O nome não pode ser nulo ou em branco. Tente novamente.");
+            continue; // Volta a pedir o nome
+        }
 
-  if (DateTime.TryParseExact(dataDeNascimento, new[] { "dd/MM/yyyy", "ddMMyy", "ddMMyyyy" }, null, System.Globalization.DateTimeStyles.None, out DateTime data))
-  {
-    dataNascimento = data; // Define a data de nascimento se a análise for bem-sucedida
-  }
-  else
-  {
-    Console.WriteLine("Formato de data inválido. Tente novamente.");
-  }
+        DateTime? dataNascimento = null;
+
+        while (dataNascimento == null)
+        {
+            Console.WriteLine("Digite a data de nascimento no formato (dd/MM/yyyy, ddMMyy ou ddMMyyyy):");
+            string dataDeNascimento = Console.ReadLine()!;
+            
+            if (string.IsNullOrWhiteSpace(dataDeNascimento))
+            {
+                Console.WriteLine("A data de nascimento não pode estar em branco. Tente novamente.");
+                continue; // Volta a pedir a data de nascimento
+            }
+
+            if (DateTime.TryParseExact(dataDeNascimento, new[] { "dd/MM/yyyy", "ddMMyy", "ddMMyyyy" }, null, System.Globalization.DateTimeStyles.None, out DateTime data))
+            {
+                dataNascimento = data; // Define a data de nascimento se a análise for bem-sucedida
+            }
+            else
+            {
+                Console.WriteLine("Formato de data inválido. Tente novamente.");
+            }
+        }
+
+        dadosPessoais.Add(nome, dataNascimento.Value);
+         contadorPessoas++;
+
+        DateTime dataCentenaria = dataNascimento.Value.AddYears(100);
+
+        TimeSpan tempoRestante = dataCentenaria - DateTime.Now;
+
+        int anosRestantes = (int)tempoRestante.TotalDays / 365;
+        int diasRestantes = (int)tempoRestante.TotalDays % 365;
+
+        Console.WriteLine($"Eu, {nome}, estarei vivo até o dia: {dataCentenaria.ToString("dd/MM/yyyy")}");
+        Console.WriteLine($"Ainda tenho {anosRestantes} anos e {diasRestantes} dias de vida.");
+    }
+    using (StreamWriter sw = new StreamWriter("dados_pessoais.txt"))
+    {
+      foreach(var pessoa in dadosPessoais)
+      {
+        sw.WriteLine($"Nome: {pessoa.Key}, {pessoa.Value.ToString("dd/MM/yyyy")}");
+      }
+    }
 }
-
-DateTime dataCentenaria = dataNascimento.Value.AddYears(100);
-
-TimeSpan tempoRestante = dataCentenaria - DateTime.Now;
-
-int anosRestantes = (int)tempoRestante.TotalDays / 365;
-int diasRestantes = (int)tempoRestante.TotalDays % 365;
-
-Console.WriteLine($"Eu, {nome}, estarei vivo até o dia: {dataCentenaria.ToString("dd/MM/yyyy")}");
-Console.WriteLine($"Ainda tenho {anosRestantes} anos e {diasRestantes} dias de vida.");
-
-
-
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um Erro: {ex.Message}");
+}
